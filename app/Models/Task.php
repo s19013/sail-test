022b9cb003fb06ba43bbfb207340a8f23a93d972
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-08 15:40:11 +0900

    ガードと､型設定

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at'
+    ];
+
+    protected $casts = [
+        'task_name' => 'string',
+    ];
+}
