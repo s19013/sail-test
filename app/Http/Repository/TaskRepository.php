1b6eb03bad5614531cd7191492281edbc9ee2774
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-09 21:50:15 +0900

    キーワード計算をできるようにした

--- app/Http/Repository/TaskRepository.php
+++ app/Http/Repository/TaskRepository.php
@@ -14,4 +14,49 @@ class TaskRepository{
     function incompleteTask() : Collection {
         return Task::where('deleted_at', null)->get();
     }
+
+    function searchIncompleteTask(Array $attributes) : Collection {
+         // 検索につかうようにエスケープしたり､空白区切りで配列化
+         $wordListToSearch = $this->preparationToAndSearch($attributes['keyword']);
+
+         $query = Task::where('deleted_at', null);
+
+        //  キーワードたちでlike検索
+         foreach($wordListToSearch as $word){$query->where('task_name','like',"%$word%");}
+
+         return $query->get();
+    }
+
+     /**sqlでlike検索する前にするエスケープ処理 */
+     public function sqlEscape($arg)
+     {
+         // %をエスケープ
+         $escaped = preg_replace(
+         '/%/',
+         '\%',
+         $arg);
+
+         // _をエスケープ
+         $escaped = preg_replace(
+         '/_/',
+         '\_',
+         $escaped);
+
+         return $escaped;
+     }
+
+     /**and検索できるように空白で区切って､配列にする */
+     public function separatedByWhiteSpace($arg){
+         // 全角スペースを半角に変換
+         $spaceConversion = mb_convert_kana($arg, 's');
+
+         // 単語を半角スペースで区切り、配列にする
+         return preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
+     }
+
+     /** エスケープして､空白区切りの配列を返す */
+     public function preparationToAndSearch($arg)
+     {
+         return $this->separatedByWhiteSpace($this->sqlEscape($arg));
+     }
 }
